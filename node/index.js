dafd0ed5982078cf9928e1c2e71e9a708959d3d6
Diego <diego.diasilva20@gmail.com>
2023-08-02 01:50:13 -0300

    Correção do index.js

--- node/index.js
+++ node/index.js
@@ -12,6 +12,8 @@ const config = {
 const mysql = require('mysql');
 const connection = mysql.createConnection(config);
 
+app.use(express.urlencoded({ extended: true })); // Parsear o corpo das requisições como formulários
+
 function createTable() {
   return new Promise((resolve, reject) => {
     const sql = `
@@ -43,18 +45,14 @@ function insertData(nome) {
   });
 }
 
-function getLastInsertedRecord() {
+function getAllRecords() {
   return new Promise((resolve, reject) => {
-    const sql = `SELECT id, nome FROM people ORDER BY id DESC LIMIT 1`;
+    const sql = `SELECT id, nome FROM people`;
     connection.query(sql, (error, results) => {
       if (error) {
         return reject(error);
       }
-      if (results.length === 0) {
-        return resolve(null);
-      }
-      const { id, nome } = results[0];
-      resolve({ id, nome });
+      resolve(results);
     });
   });
 }
@@ -62,7 +60,53 @@ function getLastInsertedRecord() {
 async function init() {
   try {
     await createTable();
-    console.log('Servidor MySQL pronto!');
+    console.log('Tabela criada com sucesso!');
+    // Definir as rotas aqui, após a criação da tabela
+
+    // Rota para exibir o formulário de inserção de nome e a lista de registros
+    app.get('/', async (req, res) => {
+      try {
+        const records = await getAllRecords();
+
+        if (records.length === 0) {
+          return res.status(404).send('Nenhum registro encontrado');
+        }
+
+        const recordsList = records
+          .map((record) => `<li>ID: ${record.id}, Nome: ${record.nome}</li>`)
+          .join('');
+
+        res.send(`
+          <h1>Full Cycle</h1>
+          <form action="/add" method="post">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome">
+            <button type="submit">Adicionar</button>
+          </form>
+          <h2>Todos os registros incluídos no banco:</h2> 
+          <ul>
+            ${recordsList}
+          </ul>
+        `);
+      } catch (error) {
+        res.status(500).send('Erro ao processar a requisição');
+      }
+    });
+
+    // Rota para receber o nome inserido e cadastrar no banco de dados
+    app.post('/add', async (req, res) => {
+      try {
+        const nome = req.body.nome;
+        await insertData(nome);
+        res.redirect('/'); // Redirecionar para a rota principal após a inserção
+      } catch (error) {
+        res.status(500).send('Erro ao processar a requisição');
+      }
+    });
+
+    app.listen(port, () => {
+      console.log('Rodando na porta ' + port);
+    });
   } catch (error) {
     console.error('Erro ao criar tabela:', error);
   }
@@ -71,30 +115,4 @@ async function init() {
 // Chamar a função init() com await aqui
 (async () => {
   await init();
-  app.listen(port, () => {
-    console.log('Rodando na porta ' + port);
-  });
 })();
-
-app.get('/', async (req, res) => {
-  try {
-    const nome = 'Diaz Diego';
-    
-    await insertData(nome);
-    
-    const record = await getLastInsertedRecord();
-    
-    if (!record) {
-      return res.status(404).send('Nenhum registro encontrado');
-    }
-    
-    res.send(`
-      <h1>Full Cycle</h1>
-      <h2>O último registro incluído no banco foi:</h2> 
-      <h2>ID: ${record.id}</h2>
-      <h2>Nome: ${record.nome}</h2>
-    `);
-  } catch (error) {
-    res.status(500).send('Erro ao processar a requisição');
-  }
-});
